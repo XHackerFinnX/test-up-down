a53e4619ea7e7cad695d79842a46c28dd81d1186
HackerFinn <hackerfinn-linux@mail.ru>
2023-11-21 16:38:43 +0300

    ok - 2

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@ int main() {
     cin >> n >> m;
     if (n > m) {
         cout << n << " > " << m << endl;
+        cout << "БОЛЬШЕ" << endl;
     } else if (m > n) {
         cout << n << " < " << m << endl;
+        cout << "МЕНЬШЕ" << endl;
     } else {
         cout << n << " = " << m << endl;
         cout << "РАВНО" << endl;
